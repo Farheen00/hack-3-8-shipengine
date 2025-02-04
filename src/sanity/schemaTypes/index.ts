5834013221e64ee002570790f0d0b02f34ed80af
Farheen00 <farheenusman2@gmail.com>
2025-02-04 14:46:27 +0500

    update

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,10 +2,9 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { productSchema } from "./products";
 import { categorySchema } from "./categories";
-import { OrderSchema } from './order'
-import { OrderItemSchema } from './orderItem'
+
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema, categorySchema,OrderSchema,OrderItemSchema],
+  types: [productSchema, categorySchema],
 };
 
